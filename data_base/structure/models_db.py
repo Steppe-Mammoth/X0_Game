011ceaa144dfcba2a4809eb5a87c5b9825b27527
Stepee-Mammoth <qwqwqww505@yahoo.com>
2022-07-24 22:47:12 +0300

    refactor classes Data Base. Model FriendsDB - added column created_on

--- data_base/structure/models_db.py
+++ data_base/structure/models_db.py
@@ -1,6 +1,4 @@
-from aiogram.types import User
 from sqlalchemy import Column, Integer, String, ForeignKey, Sequence, BigInteger, DateTime
-from datetime import datetime
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -15,20 +13,11 @@ class UserDB(Base):
     full_name = Column(String(70), nullable=False)
     created_on = Column(DateTime)
 
-    def __init__(self, user: User):
-        self.user_id = user.id
-        self.username = user.username
-        self.full_name = user.full_name
-        self.created_on = datetime.utcnow()
-
 
 class FriendsDB(Base):
     __tablename__ = 'friends'
 
     id = Column(Integer, Sequence('friends_id_seq'), primary_key=True)
-    user1_id = Column(BigInteger, ForeignKey("users.user_id"))
-    user2_id = Column(BigInteger, ForeignKey("users.user_id"))
-
-    def __init__(self, user1_id, user2_id):
-        self.user1_id = user1_id
-        self.user2_id = user2_id
+    user1_id = Column(BigInteger, ForeignKey("users.user_id", ondelete='CASCADE'))
+    user2_id = Column(BigInteger, ForeignKey("users.user_id", ondelete='CASCADE'))
+    created_on = Column(DateTime)
